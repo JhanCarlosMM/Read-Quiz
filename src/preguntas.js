eecf84203d7b1416c6fd61153a97d86aa6fc3800
THEPRINCE\The Prince <jhacarlosmena@gmail.com>
2022-09-19 19:45:04 -0500

    Se agrega el proyecto

---
+++ src/preguntas.js
@@ -0,0 +1,76 @@
+const preguntas = [
+    {
+      titulo: "¿React es el mejor lenguaje de programación?",
+      opciones: [
+        { textoRespuesta: "VERDADERO", isCorrect: true },
+        { textoRespuesta: "FALSO", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "¿React es tendencia?",
+      opciones: [
+        { textoRespuesta: "FALSO", isCorrect: false },
+        { textoRespuesta: "VERDADERO", isCorrect: true },
+      ],
+    },
+    {
+        titulo: "¿React hace uso de la API de Reconciliación?",
+        opciones: [
+          { textoRespuesta: "FALSO", isCorrect: false },
+          { textoRespuesta: "VERDADERO", isCorrect: true },
+        ],
+      },
+      {
+        titulo: "¿React hace uso de otras API?",
+        opciones: [
+          { textoRespuesta: "FALSO", isCorrect: false },
+          { textoRespuesta: "VERDADERO", isCorrect: true },
+        ],
+      },{
+        titulo: "¿La extención de React es .jxs?",
+        opciones: [
+          { textoRespuesta: "FALSO", isCorrect: false },
+          { textoRespuesta: "VERDADERO", isCorrect: true },
+        ],
+      },{
+        titulo: "¿React React es una biblioteca o un framework?",
+        opciones: [
+          { textoRespuesta: "FALSO", isCorrect: false },
+          { textoRespuesta: "VERDADERO", isCorrect: true },
+        ],
+      },{
+        titulo: "¿Las keys o claves permiten diferenciar entre elementos únicos en el DOM virtual?",
+        opciones: [
+          { textoRespuesta: "FALSO", isCorrect: false },
+          { textoRespuesta: "VERDADERO", isCorrect: true },
+        ],
+      },{
+        titulo: "¿El componente StrictMode permite identificar problemas potenciales en la aplicación.?",
+        opciones: [
+          { textoRespuesta: "FALSO", isCorrect: false },
+          { textoRespuesta: "VERDADERO", isCorrect: true },
+        ],
+      },{
+        titulo: "¿ReactDOM es una dependencia que actúa en el nivel superior de una SPA, conectando el ecosistema de React con el DOM del navegador.?",
+        opciones: [
+          { textoRespuesta: "FALSO", isCorrect: false },
+          { textoRespuesta: "VERDADERO", isCorrect: true },
+        ],
+      },
+    {
+      titulo: "¿El DOM virtual o VDOM es un patrón de React basado en abstraer el DOM del navegador?",
+      opciones: [
+        { textoRespuesta: "VERDADERO", isCorrect: true },
+        { textoRespuesta: "FALSO", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "¿En qué año fue creado JavaScript?",
+      opciones: [
+        { textoRespuesta: "1987", isCorrect: false },
+        { textoRespuesta: "1995", isCorrect: true },
+      ],
+    },
+  ];
+  
+  export default preguntas;
